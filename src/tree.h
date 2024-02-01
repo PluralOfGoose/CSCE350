fbba554ca46c360eb3286412aefc6aee484a6685
PluralOfGoose <brown.willr@gmail.com>
2024-02-01 11:28:38 -0500

    final commit

--- src/tree.h
+++ src/tree.h
@@ -1,5 +1,9 @@
 #ifndef TREE_H
 #define TREE_H
+#include <unordered_set>
+using namespace std;
+
+
 
 template<typename T>
 struct BST_Node {
@@ -32,26 +36,70 @@ void delete_tree(BST_Node<T>* root){
     delete root;
 }
 
-
 //your work starts here
 template<typename T>
 int num_nodes(BST_Node<T>* root){
-    return 0;
+    if (root == NULL){
+        return 0;
+    }
+    
+    else {
+        int left_root = num_nodes(root->left);
+        int right_root = num_nodes(root->right);
+
+        return right_root + left_root + 1;
+    }
+    
 }
 
+// template<typename T>
+// bool has_duplicate_val(BST_Node<T>* root){
+//     std::unordered_set<T> visited_values;
+//     if (root = NULL) return; 
+//     has_duplicate_val(root->left);
+//     visited_values.push(root->val);
+//     has_duplicate_val(root->right);
+    
+//     for (i=0; i < visited_values.size(); i++){
+//         if (visited_values.find())
+//     }
+//     return true;
+// }
 
 template<typename T>
-bool has_duplicate_val(BST_Node<T>* root){
-    return true;
-
+bool has_duplicate_val(BST_Node<T>* root) {
+    std::unordered_set<T> visited_values;
+    return dup_val_helper(root, visited_values);
 }
-
 
 template<typename T>
-bool trees_identical(BST_Node<T>* a, BST_Node<T>* b){
-    return true;
+bool dup_val_helper(BST_Node<T>* root, std::unordered_set<T>& visited_values) {
+    if (root == nullptr) {
+        return false;  
+    }
+
+    if (!visited_values.insert(root->val).second) {
+        return true;  
+    }
+
+    return dup_val_helper(root->left, visited_values) ||
+           dup_val_helper(root->right, visited_values);
 }
 
+template<typename T>
+bool trees_identical(BST_Node<T>* a, BST_Node<T>* b) {
+    if (a == nullptr && b == nullptr) {
+        return true;
+    }
+
+    if (a == nullptr || b == nullptr) {
+        return false;
+    }
+
+    return (a->val == b->val) &&
+           trees_identical(a->left, b->left) &&
+           trees_identical(a->right, b->right);
+}
 
 
 #endif //TREE_H
